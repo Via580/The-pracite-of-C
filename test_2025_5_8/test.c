b2df24c5fa1b0e6599ce75efcb032d9ef792fac1
Via580 <3393896775@qq.com>
2025-05-08 14:53:07 +0800

    Z字型排列

---
+++ test_2025_5_8/test.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+char*  convert(char* s, int numRows)
+{
+	char ss[100] = { 0 };
+	int len = strlen(s);
+	int t = 2 * numRows - 2;
+	int i = 0;
+	int k = 0;
+	if (numRows == 1)
+	{
+		return s;
+	}
+	for (i = 0; i < numRows; i++)
+	{
+		int j = 0;
+		for (j = 0; j + i <= len - 1; j += t)
+		{
+			ss[k++] = s[j + i];
+			if (i != 0 && i != numRows - 1 && j + t - i <= len - 1)
+				ss[k++] = s[j + t - i];
+		}
+	}
+	ss[k] = '\0';
+	return ss;
+}
+int main()
+{
+	char s[100] = "P";
+	int numRows = 0;
+	scanf("%d", &numRows);
+	printf("%s" , convert(s, numRows));
+	return 0;
+}
